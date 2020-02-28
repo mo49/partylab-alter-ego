f2d9d7e4fc9cb1047d71ca6fccaa79c7e84343df
mo49 <k.mochizuki.0409@gmail.com>
2020-02-28 13:40:03 +0900

    add  caption

--- components/atoms/Qr.vue
+++ components/atoms/Qr.vue
@@ -6,12 +6,24 @@
 div{
   @include image(300, 300);
   background-image: url('~assets/image/qr.png');
-  position: fixed;
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   @include sp-layout{
     display: none;
+  }
+  &:after{
+    content: "⬇︎ご自身のスマホで鑑賞する作品です。⬇︎";
+    display: block;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: -100px;
+    color: #fff;
+    white-space: nowrap;
+    font-size: 40px;
+    font-weight: bold;
   }
 }
 </style>
